74107efcc9768c64686f3f1648873020c371f6db
varshashaji3 <varshashaji2025@mca.ajce.in>
2025-04-10 07:10:13 +0530

    8th commit

--- freelancer/views.py
+++ freelancer/views.py
@@ -50,8 +50,6 @@ from django.contrib.auth.decorators import login_required
 from django.db import transaction
 import json
 
-import warnings
-
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
